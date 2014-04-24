65e6ac7a9792428d1936a377af786b1e072f1c8f
kingboyrang <aJia@bago.com.tw>
2014-04-24 15:42:09 +0800

    2014/04/24

--- AFWebservice/ViewController.m
+++ AFWebservice/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import "AFNetworking.h"
+#import "ServiceArgs.h"
 @interface ViewController ()
 
 @end
@@ -17,7 +18,32 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSMutableArray *params=[NSMutableArray array];
+    [params addObject:[NSDictionary dictionaryWithObjectsAndKeys:@"695749595",@"qqCode", nil]];
+    
+    ServiceArgs *args=[[ServiceArgs alloc] init];
+    args.methodName=@"qqCheckOnline";//要调用的webservice方法
+    args.soapParams=params;//传递方法参数
+    args.httpWay=ServiceHttpPost;
+    
+    /***方式一：
+    AFHTTPRequestOperation *oper=[[AFHTTPRequestOperation alloc] initWithRequest:[args request]];
+    //oper.responseSerializer=[[AFXMLParserResponseSerializer alloc] init];
+    [oper setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"xml:%@",responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+         NSLog(@"xml:%@",error);
+    }];
+    [oper start];
+     ***/
+    
+    /***方式二：***/
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager HTTPRequestOperationWithArgs:args success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"xml:%@",operation.responseString);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"error:%@", error);
+    }];
 }
 
 - (void)didReceiveMemoryWarning
